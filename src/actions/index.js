031b4ec96d9aa3d294cbad73c393f52ea09b8202
ImaiShota-ge <geborusya1@gmal.com>
2020-05-09 14:34:40 +0900

    implemant component with connect function

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,10 +1,15 @@
 export const INCREMENT = 'INCREMENT'
 export const DECREMENT = 'DECREMENT'
+export const CLEAR = 'CLEAR'
 
-export const incriment = () => ({
+export const increment = () => ({
     type: INCREMENT
 })
 
-export const decriment = () => ({
+export const decrement = () => ({
     type: DECREMENT
+})
+
+export const clear = () => ({
+    type: CLEAR
 })
--- src/reducers/count.js
+++ src/reducers/count.js
@@ -1,4 +1,4 @@
-import { INCREMENT, DECREMENT } from '../actions'
+import { INCREMENT, DECREMENT, CLEAR } from '../actions'
 
 const initialState = { value: 0 }
 
@@ -8,6 +8,8 @@ export default (state = initialState, action) => {
             return { value: state.value + 1 }
         case DECREMENT:
             return { value: state.value - 1 }
+        case CLEAR:
+            return { value: state.value = 0 }
         default:
             return state
     }
